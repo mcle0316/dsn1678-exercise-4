93423ed23671dddcad8db93111294bddf953f669
Jenna <Jenna@Jennas-MacBook-Pro.local>
2014-10-07 10:25:26 -0400

    spaceship slide up section

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var $sun = $('.sun');
 var $sunSection = $('.sun-section');
 var $dipperSection = $('.dipper-section');
 var $dipper = $('.dipper');
+var $shuttle = $('.shuttle');
+var $shuttleSection = $('.shuttle-section');
 
 
 $win.on('scroll', function () {
@@ -10,7 +12,8 @@ $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
 	
 	$sun.css('transform','rotate(' + scrollPos / 5 + 'deg)'); 
-	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px'); 
+	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
+	 
 });
 
 $dipperSection.waypoint(function () {
@@ -19,3 +22,13 @@ $dipperSection.waypoint(function () {
 	/*console.log('Visible!'); */
 	
 }, { offset: '50%' });
+
+
+$shuttleSection.waypoint(function () {
+	$shuttle.addClass('js-shuttle-fade');
+	
+	/*console.log('Visible!'); */
+	
+}, { offset: '50%' });
+
+
