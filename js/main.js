6d89d855882b34c2ba716497621fe93104a36980
Jenna <Jenna@Jennas-MacBook-Pro.local>
2014-10-07 08:36:19 -0400

    sun rotation start

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var $win = $(window); 
+var $sun = $('.sun');
+
+$win.on('scroll', function () {
+	
+	var scrollPos = $win.scrollTop();
+	
+	$sun.css('transform','rotate(' + scrollPos + 'deg)');
+	
+	
+});
